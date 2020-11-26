532a38d48350cdaf5d2c60d618dd0e0aee9cf642
PragatiVerma18 <itispragativerma@gmail.com>
2020-11-26 13:12:09 +0530

    Fixed Gradle Errors

--- nonCisMaleInCs/index.js
+++ nonCisMaleInCs/index.js
@@ -1,7 +1,9 @@
 /**
  * @format
  */
+import {start} from './src/Navigation';
 
+start();
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
