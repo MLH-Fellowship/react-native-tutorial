3482aa38851f53e3b02ef6273a9132eccd086e44
Temitope Akinsoto <takinsoto@gmail.com>
2020-11-28 09:38:49 +0100

    remove commented-out codes and clean up css rules

--- nonCisMaleInCs/DetailsView.js
+++ nonCisMaleInCs/DetailsView.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image, Button, Alert, TouchableOpacity} from 'react-native';
-// import { Icon } from 'react-native-elements' 
-// import { Header } from 'react-native-elements';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-import editIcon from './edit.png';
-import nophoto from './nophoto.png';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Alert,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import editIcon from './assets/images/edit.png';
+import nophoto from './assets/images/nophoto.png';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -15,42 +20,39 @@ const onPressed = () => {
 const DetailsView = () => {
   return (
     <>
-      <View style={styles.menuContainerStyle}>
-        <Text style={styles.menuText}>{'< Home'}</Text>
-      </View>
-      <View style={styles.headerImageContainer}>
-        <Image style={styles.imageStyle} source={nophoto} />
-      </View>
-      <View style={styles.body}>
-        <View style={styles.titleContainer}>
+      <Text style={styles.menuText}>{'< Home'}</Text>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.headerImageContainer}>
+          <Image style={styles.imageStyle} source={nophoto} />
+        </View>
+        <View style={styles.body}>
           <Text style={styles.titleStyle}>JOHN DOE</Text>
-        </View>
-        <View style={styles.sectionContainer}>
           <Text style={styles.sectionTitle}>Introduction</Text>
-        </View>
-        <View style={styles.sectionContainer}>
           <Text style={styles.sectionTitle}>Contribution</Text>
-        </View>
-        <View style={styles.sectionContainer}>
           <Text style={styles.sectionTitle}>Domain ( Tag form )</Text>
         </View>
-      </View>
-      <TouchableOpacity onPress={onPressed} style={styles.appButtonContainer}>
-        <Image source={editIcon} style={styles.appButtonIcon} />
-        <Text style={styles.appButtonText}>Edit Button</Text>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={onPressed} style={styles.appButtonContainer}>
+          <Image source={editIcon} style={styles.appButtonIcon} />
+          <Text style={styles.appButtonText}>EDIT BUTTON</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-   menuContainerStyle: {
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  menuContainerStyle: {
     backgroundColor: '#3D6DCC',
     padding: 25,
-   },
-  menuText: {    
+  },
+  menuText: {
     color: Colors.white,
-    fontSize: 23    
+    fontSize: 23,
+    backgroundColor: '#3D6DCC',
+    padding: 25,
   },
   headerImageContainer: {
     alignItems: 'center',
@@ -59,67 +61,43 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lighter,
     height: 200,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     flexDirection: 'column',
-    marginBottom: 5
-  },
-  titleContainer: {
-    marginTop: 32,
-    alignItems: 'center',
+    marginBottom: 10,
   },
   titleStyle: {
     fontSize: 40,
     fontWeight: '600',
     color: Colors.black,
-  },
-  sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    alignSelf: 'center',
   },
   sectionTitle: {
     fontSize: 22,
     fontWeight: '600',
     color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  buttonContainer: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  appButtonIcon: {
-    height: 30,
-    width: 30,
-    marginRight: 24   
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
   appButtonContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    justifyContent: 'flex-start',
-    margin: 25,    
+    marginHorizontal: 25,
+    marginVertical: 30,
     padding: 15,
     backgroundColor: '#3D6DCC',
   },
   appButtonText: {
     fontSize: 18,
     height: 30,
-    color: Colors.white,    
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase",
-    marginLeft: 60
-  }
+    color: Colors.white,
+    fontWeight: 'bold',
+    marginLeft: 60,
+  },
+  appButtonIcon: {
+    height: 30,
+    width: 30,
+    marginRight: 24,
+  },
 });
 
 export default DetailsView;
