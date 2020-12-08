6de70af9dcf7c95f7a6c82e79046b59580ceeb0b
Temitope Akinsoto <takinsoto@gmail.com>
2020-12-08 17:09:11 +0100

    set up globals (beforeEach and afterEach) for reacreating wrapper for each test

--- nonCisMaleInCs/__tests__/Details-view-test.js
+++ nonCisMaleInCs/__tests__/Details-view-test.js
@@ -2,13 +2,22 @@
 import 'react-native';
 import React from 'react';
 import DetailsView from '../DetailsView';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import * as rtl from '@testing-library/react-native';
+
+afterEach(() => {
+    renderer.cleanup;
+});
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = renderer.create(<DetailsView />)
+})
 
 describe('Details View Component', () => {
-    test('renders correctly', () => {
-        renderer.create(<DetailsView />);
+    test.only('UI renders correctly', () => {
+        
     });
 });
 
