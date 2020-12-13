f5ac44c8bd66ba182ab8f70d34882a60787124ab
PragatiVerma18 <itispragativerma@gmail.com>
2020-12-13 16:10:14 +0530

    pick image added

--- nonCisMaleInCs/src/components/Form.js
+++ nonCisMaleInCs/src/components/Form.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {launchImageLibrary} from 'react-native-image-picker';
 import {
   Text,
   StyleSheet,
@@ -7,9 +8,40 @@ import {
   Button,
   Alert,
   SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
 
 const Form = () => {
+  const [imageSource, setImageSource] = useState(null);
+
+  function selectImage() {
+    const options = {
+      title: 'You can choose one image',
+      mediaType: 'photo',
+      maxWidth: 256,
+      maxHeight: 256,
+      storageOptions: {
+        skipBackup: true,
+      },
+    };
+
+    launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+        Alert.alert('You did not select any image');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        let source = {uri: response.uri};
+        console.log(source);
+
+        // ADD THIS
+        setImageSource(source.uri);
+      }
+    });
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.heading}> Add Profile </Text>
@@ -26,7 +58,11 @@ const Form = () => {
         <Text style={styles.formLabel}> Domains </Text>
         <TextInput placeholder="Enter Domains" style={styles.inputStyle} />
         <Text style={styles.formLabel}> Image </Text>
-        <TextInput placeholder="Select Image" style={styles.selectimgStyle} />
+        <TouchableOpacity>
+          <Text onPress={selectImage} style={styles.pickImage}>
+            Pick an Image
+          </Text>
+        </TouchableOpacity>
         <Button
           title="Submit"
           raised
@@ -60,17 +96,24 @@ const styles = StyleSheet.create({
     width: 300,
     paddingHorizontal: 15,
     borderRadius: 5,
-    backgroundColor: '#F2F3F4',
+    borderColor: '#d1d5db',
+    borderWidth: 1,
     lineHeight: 26,
   },
-  selectimgStyle: {
+  pickImage: {
+    marginBottom: 30,
     marginTop: 5,
     width: 300,
     paddingHorizontal: 25,
-    marginBottom: 30,
+    paddingVertical: 25,
     borderRadius: 5,
+    borderColor: 'black',
     backgroundColor: '#F2F3F4',
+    borderWidth: 1,
+    borderStyle: 'dashed',
     lineHeight: 26,
+    color: 'black',
+    textAlign: 'center',
   },
   formText: {
     alignItems: 'center',
