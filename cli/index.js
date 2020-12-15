fd276d5a4d53549d904240e8bc696048d5118c8f
Liang Liu <stuffbyliang@gmail.com>
2020-12-15 05:10:54 -0800

    added cli commands to go to next step

--- cli/index.js
+++ cli/index.js
@@ -8,7 +8,38 @@ const program = new Command()
 const pkgJson = require('./package.json')
 
 /**
+ * @type {string[]} - list of commit hashes in order
+ *                    each commit should test the required solution for that tutorial step.
+ */
+const steps = [
+  'hash1',
+  'hash2'
+]
+
+/**
+ * Returns the next step's commit hash
+ * 
+ * @returns {string}
+ */
+async function getNextStepsCommitHash() {
+  let commits = await getCommitHistory()
+
+  // loop through each commit starting at the latest one and match that
+  // to the commits in the steps array to see what step we are in
+  for(let commit of commits) {
+    let index = steps.indexOf(commit)
+
+    if(index >= 0) {
+      return steps[index + 1];
+    }
+  }
+
+  return null
+}
+
+/**
  * Returns a list of remote branches
+ * 
  * @returns {string[]}
  */
 async function getRemoteBranches() {
@@ -17,10 +48,29 @@ async function getRemoteBranches() {
 
 /**
  * Returns the current branch
+ * 
  * @returns {string}
  */
 async function getCurrentBranch() {
     return (await git.raw(['rev-parse', '--abbrev-ref', 'HEAD'])).trim()
+}
+
+/**
+ * Returns list of recent commit hashes
+ * 
+ * @returns {string[]}
+ */
+async function getCommitHistory() {
+  return (await git.raw(['rev-list', 'HEAD'])).trim().split('\n')
+}
+
+/**
+ * Cherry picks the specified commit hash onto current branch
+ * 
+ * @param  {string} hash
+ */
+async function cherryPick(hash) {
+  return (await git.raw(['cherry-pick', hash])).trim()
 }
 
 /**
@@ -78,6 +128,29 @@ program
   .description('List out current branch')
   .action(async (source, destination) => {
     console.log(chalk.cyan(`Current branch: ${await getCurrentBranch()}`))
+  })
+
+/*
+  Adds the next step's commit to the current branch
+*/
+program
+  .command('next')
+  .description('Adds next step to the current branch')
+  .action(async (source, destination) => {
+    let hash = await getNextStepsCommitHash()
+    await cherryPick(hash)
+    console.log(chalk.cyan(`Successfully moved on to the next step!`))
+  })
+
+/*
+  test
+*/
+program
+  .command('test')
+  .description('test')
+  .action(async (source, destination) => {
+    let hash = await getNextStepsCommitHash()
+    await cherryPick(hash)
   })
   
 program.parse(process.argv)
